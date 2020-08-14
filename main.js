94cbb8a276d7a1869473cf6985f13f4e32be7c6d
tnt84268 <dannynguyen84268@gmail.com>
2020-08-14 14:35:14 -0700

    Create render

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Notification } = require('electron')
+const { app, BrowserWindow, Notification, ipcMain } = require('electron')
 const https = require('https');
 
 function createWindow () {
@@ -22,8 +22,15 @@ app.on('ready', () => {
     //new Notification("testing", { body: "test" });
 });
 
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'pong')
+})
 
-
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
 
 https.get('https://dublinusd.instructure.com/api/v1/courses?access_token=<ACCESS-TOKEN>', (resp) => {
   let data = '';
