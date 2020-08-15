4aa4cb9ada11e6d2e10b986a764f2a0aa5200d9e
tnt84268 <dannynguyen84268@gmail.com>
2020-08-15 15:24:30 -0700

    Change request stuff

--- render.js
+++ render.js
@@ -1,21 +1,28 @@
-const { ipcRenderer } = require('electron')
-//console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+const { ipcRenderer } = require("electron");
+//console.log(ipcRenderer.sendSync("synchronous-message", "ping")) // prints "pong"
 
-//ipcRenderer.on('asynchronous-reply', (event, arg) => {
+//ipcRenderer.on("asynchronous-reply", (event, arg) => {
 //  console.log(arg) // prints "pong"
 //})
-//ipcRenderer.send('asynchronous-message', 'ping')
+//ipcRenderer.send("asynchronous-message", "ping")
 
-document.getElementById("submit").addEventListener("click", submitToken);
-
-function submitToken() {
-  ipcRenderer.send('courses', document.getElementById("apiToken").value);
+function requestData(resource, data) {
+    ipcRenderer.send(resource, data);
 }
 
-ipcRenderer.on('courses', (event, arg) => {
-  if (arg){
-    console.log('it works')
-  }
-  console.log(arg)
-})
+// Login listener
+document.getElementById("submit").addEventListener("click", function(){ requestData("courses", document.getElementById("apiToken").value);});
+
+//Dashboard listeners
+//document.getElementById("idhere").addEventListener("click", function(){ functionName(args);});
+//document.getElementById("idHere").addEventListener("click", function(){ functionName(args);});
+//document.getElementById("idhere").addEventListener("click", function(){ functionName(args);});
+//document.getElementById("idHere").addEventListener("click", function(){ functionName(args);});
+
+ipcRenderer.on("courses", (event, jsonData) => {
+    console.log(jsonData);
+    console.log("test");
+});
+
+
 
