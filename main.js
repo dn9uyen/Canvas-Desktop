47e5f8c215b893279e64dbd6bc34b05b8b4cbcf8
tnt84268 <dannynguyen84268@gmail.com>
2020-08-17 18:55:54 -0700

    Fix bug from last commit

--- main.js
+++ main.js
@@ -20,27 +20,28 @@ app.on('ready', () => {
 });
 
 ipcMain.on("openNewPage", (event, page) => {
-    win.loadURL("file://" + __dirname + page + ".html");
+    win.loadURL("file://" + __dirname + "/" + page + ".html");
 });
 
 // TODO: add to settings file
-global.token = "";
 global.loggedIn = false;
 
 // Request json data
-ipcMain.on("jsonData", (event, args) => {
+ipcMain.on("jsonData", (event, resourceAndToken) => {
     // args[0] is resource name, arg[1] is token
-    requestCanvas(args[0], function(json) {
-        if (!global.loggedIn) {global.token=args[1];}
+    requestCanvas(resourceAndToken, function(json) {
         // return resource string
-        event.reply("jsonData", json, args[1]);
+        event.reply("jsonData", json, resourceAndToken[0]);
     });
     
 });
 
-function requestCanvas(resource, callback) {
+function requestCanvas(resourceAndToken, callback) {
     // request canvas with the api location
-    const url = "https://dublinusd.instructure.com/api/v1/"+resource+"?access_token="+global.token;
+    console.log(global.token);
+    console.log(resourceAndToken[0]);
+    console.log(resourceAndToken[1]);
+    const url = "https://dublinusd.instructure.com/api/v1/"+resourceAndToken[0]+"?access_token="+resourceAndToken[1];
     https.get(url, (response) => {
         if (response.statusCode==200) {
             console.log("valid request");
--- render.js
+++ render.js
@@ -12,10 +12,10 @@ var _listener = function() {
 document.getElementById("submit").addEventListener("mousedown", _listener);
 
 // Handle response
-ipcRenderer.on("jsonData", (event, args) => {
-    // args[0] is json data, args[1] is resource name
-    ipcRenderer.send("openNewPage", args[1]);
-    if (!global.loggedIn) {global.loggedIn = true; document.getElementById("submit").removeEventListener("mousedown", _listener);}
+ipcRenderer.on("jsonData", (event, jsonData, resource) => {
+    ipcRenderer.send("openNewPage", resource);
+    console.log(jsonData);
+    // if (!global.loggedIn) {global.loggedIn = true; document.getElementById("submit").removeEventListener("mousedown", _listener);}
     // handle ui events here
 });
 
